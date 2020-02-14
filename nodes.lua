134172d2266af201a0e36306d32005bfbc4c9430
poikilos <7557867+poikilos@users.noreply.github.com>
2020-02-13 22:13:36 -0500

    work on textures

--- nodes.lua
+++ nodes.lua
@@ -2,13 +2,13 @@
 colorapi.register_colorapi_node("colorapi:wool", {
     description   = "Wool"     ,
     drawtype      = "colorfacedir", -- or not--this is just an experiment; See "morecolorfacedir" in colorapi/README.md
-    tiles         = lava_crust_source_tiles       ,
-    paramtype     = "none",
+    tiles         = {'colorapi_wool'},
+    -- paramtype     = "none",
     buildable_to = true,
     diggable = true,
     pointable = true,
     walkable = true,
     is_ground_content = false,
-    drop              = "onewool:onewool",
+    drop              = "colorapi:wool",
     groups            = { oddly_breakable_by_hand = 1 } ,
 })
